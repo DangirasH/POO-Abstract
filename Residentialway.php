266d5cf68a94453fc55d3ff789cbfaefb8c36b9b
DangirasH <97556742+DangirasH@users.noreply.github.com>
2022-04-03 23:12:51 +0200

    Add files via upload

---
+++ Residentialway.php
@@ -0,0 +1,24 @@
+<?php
+
+
+require_once 'HighWay.php';
+require_once 'Vehicle.php';
+require_once 'Car.php';
+require_once 'Bicycle.php';
+require_once 'Truck.php';
+
+
+
+final class ResidentialWay extends HighWay
+{
+    protected int $maxSpeed = 50;
+    protected int $nbLane = 2;
+
+    public function addVehicle(Vehicle $vehicle): string
+    {
+        $this->setCurrentVehicles($vehicle);
+
+        return 'vehicle added';
+    }
+
+}
---
+++ Truck.php
@@ -0,0 +1,61 @@
+<?php
+
+
+require_once 'Vehicle.php';
+
+class Truck extends Vehicle
+{
+
+    protected int $stockCapacity;
+    protected int $stockLoaded = 0;
+
+    public function __construct(string $color, int $nbSeats, int $stockCapacity)
+
+    {
+        parent::__construct($color, $nbSeats);
+
+        $this->stockCapacity = $stockCapacity;
+
+    }
+    public function isFull()
+
+    {
+        if ($this->getStockCapacity() > 0) {
+
+            echo "in filling";
+
+        } elseif ($this->getStockCapacity() <= 0) {
+
+            echo "full";
+
+        }
+
+    }
+
+    public function getStockLoaded(): int
+    {
+        return $this->stockLoaded;
+    }
+
+
+    public function getStockCapacity(): int
+    {
+        return $this->stockCapacity;
+    }
+
+
+
+    public function setStockLoaded(int $stockLoaded): void
+    {
+
+        $this->stockLoaded = $stockLoaded;
+    }
+
+
+    public function setStockCapacity(int $stockCapacity): void
+    {
+
+        $this->stockCapacity = $stockCapacity;
+    }
+
+}
